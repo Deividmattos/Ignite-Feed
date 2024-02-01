308b8428f431aa8cc949e4f3de352dbb9d04f807
Deividmattos <deividmattos79@hotmail.com>
2024-02-01 21:53:06 +0000

    Create Img

--- ignite/aulas/src/App.jsx
+++ ignite/aulas/src/App.jsx
@@ -1,22 +1,33 @@
-import { Post } from './assets/Post'
-import './assets/global.css'
+import { Post } from './assets/Post';
+import './assets/global.css';
+import { Sidebar } from './components/Sidebar';
+import styles from './components/Sidebar.module.css'
+
 import { Header } from './components/Header';
 
 
  export function App() {
   return (
     <div>
+       <Header/>
 
-     <Header/>
+       <div className={styles.wrapper}>
+         <aside>
+          <Sidebar/>
+         </aside>
 
-      <Post
-      author="Deivid de Mattos"
-      content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-      />
-      <Post
-      author="Deivid de Mattos Silva"
-      content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here"
-      />
+         <main>
+           <Post
+             author="Deivid de Mattos"
+             content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+           />
+           <Post
+              author="Deivid de Mattos Silva"
+              content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here"
+           />
+         </main>
+
+       </div>
       </div>
     
   );
