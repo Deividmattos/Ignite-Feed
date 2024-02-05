4f08ad7f6ff3075d040cbd22f2934fc36c8c0509
Deividmattos <deividmattos79@hotmail.com>
2024-02-05 21:44:00 +0000

    update card

---
+++ ignite/aulas/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+        <img src="https://github.com/Deividmattos.png" alt="" />
+
+        <div className={styles.commentBox}>
+           <div className={styles.commentContent}>
+              <header>
+                <div className={styles.authorAndTime}>
+                  <strong>Deivid de Mattos</strong>
+                  <time title="03 de Fevereiro as 12:34 "dateTime="2022-05-11 08:13:30">Cerca de uma ha 1ha traz</time>
+                </div>
+
+                <button title="Deletar comentario">
+                  
+                </button>
+              </header>
+
+              <p>Muitoss bom Devon, parabens!!</p>
+                 </div>  
+            <footer>
+              <button>
+                 
+                 Aplaudir <span>20</span>
+              </button>
+            </footer>
+           
+
+        </div>
+    </div>
+  )
+}
+
--- ignite/aulas/src/components/Post.jsx
+++ ignite/aulas/src/components/Post.jsx
@@ -1,19 +1,47 @@
-import styles from './Post.module.css'
+import { Comment } from './Comment';
+import styles from './Post.module.css';
 
 export function Post() {
   return(
-      <article classAName={styles.Post}>
+      <article className={styles.post}>
             <header>
-                <div classAName={styles.author}>
-                  <img classAName={styles.avatar} src="" alt="" /> 
-                   <div classAName={styles.authorInfo}>
-                     <strong>Deivid de Mattos Silva</strong>
+                <div className={styles.author}>
+                  <img className={styles.avatar} src="https://github.com/Deividmattos.png" alt="" /> 
+                   <div className={styles.authorInfo}>
+                     <strong>Deivid de Mattos </strong>
                      <span>Web Developer</span>
                     </div>
                 </div>
 
-             <time dateTime="">Publicado ha 1h</time>
+               <time title="03 de Fevereiro as 12:34 "dateTime="2022-05-11 08:13:30">Publicado ha 1h</time>
             </header>
+
+            <div className={styles.content}>
+           
+               <p>Fala galeraa 👋</p>
+               <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+               <p><a href="">👉 jane.design/doctorcare</a></p>
+
+                 <p>
+                    <a href="">#Novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rockeseat</a>
+                  </p>   
+            </div>
+
+            <form className={styles.commentForm}>
+                    <strong>Deixe seu feedback</strong>
+
+                    <textarea placeholder="Deixe um comentario" />
+                    <footer>
+                       <button type="submit">Publicar</button>
+                    </footer>  
+            </form>
+            <div className={styles.commentList}>
+               <Comment/>
+
+            </div>
+            
       </article>
   )
 }
